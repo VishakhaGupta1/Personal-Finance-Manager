6ffb365d64d773e7fb67765d161a7875aba878ab
Vishakha Gupta <vishakhagupta2551@gmail.com>
2026-01-18 23:55:38 +0530

    Delete OneDrive/Desktop/demo directory

--- OneDrive/Desktop/demo/demo/src/main/java/Finance/demo/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package Finance.demo.repositories;
-
-import Finance.demo.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- OneDrive/Desktop/demo/demo/src/main/java/Finance/demo/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package Finance.demo.repositories;
-
-
-import Finance.demo.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  User findByEmailAndPassword(String email, String password);
-}
